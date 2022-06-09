454070140b8da586292ef498b220a5eb23082a12
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-06-09 11:00:33 -0400

    add questions with inquirer

--- index.js
+++ index.js
@@ -1,13 +1,93 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const Choices = require("inquirer/lib/objects/choices");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub username? (Required)",
+            validate: usernameInput => {
+                if (usernameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address? (Required)"
+        },
+        {
+            type: "input",
+            name: "project",
+            message: "What is your project name? (Required)",
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please provide a description for your project. (Required)",
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please add a description of your project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "list",
+            name: "liscense",
+            message: "What type of liscense should your project have?",
+            choices: ["MIT"],
+            default: "MIT"
+        },
+        {
+            type: "list",
+            name: "instal dependencies",
+            message: "What type of command should be run to instal dependencies?",
+            choices: ["npm i"],
+            default: "npm i"
+        },
+        {
+            type: "list",
+            name: "tests",
+            message: "What type of command should be run to run tests?",
+            choices: ["npm test"],
+            default: "npm choices"
+        },
+        {
+            type: "input",
+            name: "tips",
+            message: "What does the user need to know about using the repo?."
+        }
+    ]).then(answers => console.log(answers))
+}
 
 // Function call to initialize app
 init();
+
